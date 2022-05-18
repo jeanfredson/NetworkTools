da82411308dd0601630078340da666a94d436e2a
jeanfredson <jeanfredson@me.com>
2022-05-18 18:12:44 -0400

    Adicionado parametros para IP

--- Scripts/Bash/sping.sh
+++ Scripts/Bash/sping.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 rm /tmp/xping.sh
-
-echo "#!/bin/bash" >> /tmp/xping.sh
-chmod +x /tmp/xping.sh
+if [ "$1" == ""]
+then
+    echo "SuperPing - UltraFast"
+    echo "How to use: $0 REDE"
+    echo "Usage: ./sping 10.20.0" 
+else
+  echo "#!/bin/bash" >> /tmp/xping.sh
+  chmod +x /tmp/xping.sh
 for hosts in {1..254};
-do
-  echo "ping -w 1 -c 1 192.168.2.$hosts " '| grep "64 bytes" & ' >> /tmp/xping.sh
-done
+ do
+   echo "ping -w 1 -c 1 $1.$hosts " '| grep "64 bytes" & ' >> /tmp/xping.sh
+ done
 
-echo "wait" >> /tmp/xping.sh
-bash /tmp/xping.sh
+ echo "wait" >> /tmp/xping.sh
+ 
+ bash /tmp/xping.shd
+fi
 
  
